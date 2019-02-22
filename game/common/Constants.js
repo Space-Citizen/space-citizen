9f6abd1ce10915973450ec4acbc5f993ead565d3
Arnaud Alies <arnaudalies.py@gmail.com>
2019-02-22 15:24:40 -0800

    Moving background

--- game/common/Constants.js
+++ game/common/Constants.js
@@ -4,7 +4,7 @@ var port = 12345;
 var Constants = {
   "FRAMERATE": 30,
   "SERVER": ':' + port, // used by client (io.connect)
-  "WORLD_SIZE": 10000,
+  "WORLD_SIZE": 500,
   "PORT": port,
   "SCREEN_RATIO": 16 / 9,
   "X_VIEW_RANGE": 300, // value in meters. Y view range is defined by screen ratio
