3de481b200ae9c6397676b3a270fbcea493411f8
Arnaud Aliès <arnaudalies.py@gmail.com>
2019-02-26 09:27:19 +0000

    Wip game (#11)
    * jet flame
    
    * wip thrust
    
    * force sprites to be divisible by the number of frames
    
    * Asgard spaceship
    
    * new spaceships
    
    * wip space ships
    
    * proper kill
    
    * Moving background
    
    * Update Constants.js
    
    * wip
    
    * wip
    
    * Update ressources.js
    
    * wip ships
    
    * wip fighter
    
    * Update ressources.js
    
    * wip spaceships
    
    * Update ressources.js
    
    * wip user info
    
    * fix sg
    
    * wip sg
    
    * teleportTo function
    
    * fix reticle
    
    * wip
    
    * wip
    
    * Update BaseServerEntity.js
    
    * working event new entity
    
    * wip
    
    * server ent must call onInit
    
    * Update ServerEntityMissile.js
    
    * dict keys dont need quotes
    
    * wip ship types
    
    * ship >> ServerShip
    
    * working ship
    
    * wip ship

--- game/common/Constants.js
+++ game/common/Constants.js
@@ -2,15 +2,15 @@
 var port = 12345;
 
 var Constants = {
-  "FRAMERATE": 30,
-  "SERVER": ':' + port, // used by client (io.connect)
-  "WORLD_SIZE": 500, // If you want to edit this value:
+  FRAMERATE: 30,
+  SERVER: ':' + port, // used by client (io.connect)
+  WORLD_SIZE: 500, // If you want to edit this value:
   //world size and background size are related
-  "PORT": port,
-  "SCREEN_RATIO": 16 / 9,
-  "X_VIEW_RANGE": 300, // value in meters. Y view range is defined by screen ratio
-  "SOUND_RANGE": 300,
-  "HEALTH_BAR_SIZE": 30 // value in meters
+  PORT: port,
+  SCREEN_RATIO: 16 / 9,
+  X_VIEW_RANGE: 300, // value in meters. Y view range is defined by screen ratio
+  SOUND_RANGE: 300,
+  HEALTH_BAR_SIZE: 30 // value in meters
 }
 Object.freeze(Constants);
 
