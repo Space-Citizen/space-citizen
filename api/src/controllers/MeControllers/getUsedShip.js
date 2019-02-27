64e0d7098214f1c626a5630949ab43435d305b2d
antoine <antoine.gannat@gmail.com>
2019-02-26 23:24:33 -0800

    server: rename files. website: add possibility to select the active ship

--- api/src/controllers/MeControllers/getUsedShip.js
+++ api/src/controllers/MeControllers/getUsedShip.js
@@ -3,7 +3,7 @@ var { doQuery, mysqlError } = require('../../database/');
 // get user's ships
 module.exports = function (req, res, next, userId) {
     // get user's ships
-    doQuery("SELECT ships.`type`, items.`name` FROM `ships` INNER JOIN `items` ON `items`.id = ships.`type` WHERE ships.`owner` = ? AND ships.`in_use` = ?",
+    doQuery("SELECT ships.`id`,ships.`type`, items.`name` FROM `ships` INNER JOIN `items` ON `items`.id = ships.`type` WHERE ships.`owner` = ? AND ships.`in_use` = ?",
         [userId, true]).then(function (ship) {
             if (ship.length === 0) {
                 res.status(400).json({ error: "No ship found" });
