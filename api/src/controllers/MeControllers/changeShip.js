1a2f6afb402a2bdb33f51e95ccd45b0374054465
antoine <antoine.gannat@gmail.com>
2019-02-26 23:05:40 -0800

    server: rename files. website: add possibility to select the active ship

--- api/src/controllers/MeControllers/changeShip.js
+++ api/src/controllers/MeControllers/changeShip.js
@@ -6,11 +6,15 @@ module.exports = function (req, res, next, userId) {
         res.status(400).json({ error: "Missing arguments" });
         return;
     }
-    doQuery("UPDATE `ships` SET `in_use` = ? WHERE `id` = ? AND `owner` = ?", [1, req.body.shipId, userId]).then(function (result) {
-        if (result.affectedRows === 0) {
-            res.status(400).json({ error: "No rows updated" });
-            return;
-        }
-        res.status(200).json({ success: "Used ship changed" });
+    // reset to 0 the 'in_use' field for every other ships
+    doQuery("UPDATE `ships` SET `in_use` = ? WHERE `owner` = ?", [0, userId]).then(function (result) {
+        // set to 1 the 'in_use' field for the requested ship
+        doQuery("UPDATE `ships` SET `in_use` = ? WHERE `id` = ? AND `owner` = ?", [1, req.body.shipId, userId]).then(function (result) {
+            if (result.affectedRows === 0) {
+                res.status(400).json({ error: "No rows updated" });
+                return;
+            }
+            res.status(200).json({ success: "Current ship changed" });
+        }, function (error) { mysqlError(res, error) });
     }, function (error) { mysqlError(res, error) });
 }
