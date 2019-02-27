c798aedeb4de4557911f4203f15e352edb394263
Arnaud Aliès <arnaudalies.py@gmail.com>
2019-02-27 00:09:20 +0000

    Wip game (#13)
    * Update BC304.js
    
    * ONeill ship
    
    * Update server.js
    
    * oneill new thruster
    
    * BC304 thrusters

--- game/server/ship/ServerONeill.js
+++ game/server/ship/ServerONeill.js
@@ -2,11 +2,11 @@
 var ServerBaseShip = require("./ServerBaseShip");
 
 class ServerONeill extends ServerBaseShip {
+
     getSpeed() {
         // m/s
         return 100;
     }
-
 
     getType() {
         return "ONeill";
