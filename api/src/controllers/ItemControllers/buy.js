95fed834679b58a576a982983b691585399d2c02
antoine <antoine.gannat@gmail.com>
2019-02-27 00:19:54 -0800

    api: add waterfall to the buy function

--- api/src/controllers/ItemControllers/buy.js
+++ api/src/controllers/ItemControllers/buy.js
@@ -1,15 +1,57 @@
 var { doQuery, mysqlError } = require('../../database/');
+var waterfall = require('async-waterfall');
 
-function getItemInfo(itemId) {
-    return (doQuery("SELECT id, price, name, specifications FROM `items` WHERE `id` = ?", [itemId]));
+// get item's informations from the api
+function getItemInfo(itemId, waterfallNext) {
+    doQuery("SELECT id, price, name, specifications FROM `items` WHERE `id` = ?", [itemId]).then(function (itemInfo) {
+        if (itemInfo.length === 0) {
+            waterfallNext("Item not found");
+            return;
+        }
+        //get first element in array
+        itemInfo = itemInfo[0];
+        // call next function
+        waterfallNext(null, itemInfo);
+    }, function (error) { waterfallNext(error) });
 }
 
-function getUserBalance(userId) {
-    return (doQuery("SELECT `money` FROM `users` WHERE `id` = ?", [userId]));
+function getUserBalance(userId, itemInfo, waterfallNext) {
+    doQuery("SELECT `money` FROM `users` WHERE `id` = ?", [userId]).then(function (userBalance) {
+        if (userBalance.length === 0) {
+            waterfallNext("Your account doesn't seems to exist, contact an administrator");
+            return;
+        }
+        //get first element in array with money attribute
+        userBalance = userBalance[0].money;
+
+        // check if user has enough money
+        if (userBalance < itemInfo.price) {
+            waterfallNext("You don't have enough money to buy this item.");
+            return;
+        }
+        // update user's balance
+        var newBalance = userBalance - itemInfo.price;
+        // call next function
+        waterfallNext(null, itemInfo, newBalance);
+    }, function (error) { waterfallNext(error) });
 }
 
-function updateUserBalance(newBalance, userId) {
-    return (doQuery("UPDATE `users` SET `money` = ? WHERE `id` = ?", [newBalance, userId]));
+function updateUserBalance(itemInfo, newBalance, userId, waterfallNext) {
+    doQuery("UPDATE `users` SET `money` = ? WHERE `id` = ?", [newBalance, userId]).then(function (result) {
+        //if item is a ship
+        if (JSON.parse(itemInfo.specifications).type === "ship") {
+            // add ship to user ships
+            addNewShip(itemInfo, userId).then(function (result) {
+                waterfallNext(null, { success: itemInfo.name + " added to your hangar", newBalance: newBalance, itemId: result.insertId });
+            }, function (error) { waterfallNext(error) });;
+        }
+        else {
+            // add item to user's inventory
+            addItemToInventory(itemInfo, userId).then(function (result) {
+                waterfallNext(null, { success: itemInfo.name + " added to your inventory", newBalance: newBalance, itemId: result.insertId });
+            }, function (error) { waterfallNext(error) });;
+        }
+    }, function (error) { waterfallNext(error) });
 }
 
 function addNewShip(shipInfo, userId) {
@@ -26,47 +68,25 @@ function addItemToInventory(itemInfo, userId) {
 
 //buy an item
 module.exports = function (req, res, next, userId) {
-
-    // get item information
-    getItemInfo(req.params.itemId).then(function (itemInfo) {
-        if (itemInfo.length === 0) {
-            res.status(400).json({ error: "Item not found" });
-            return;
-        }
-        //get first element in array
-        itemInfo = itemInfo[0];
-
-        // get user's balance
-        getUserBalance(userId).then(function (userBalance) {
-            if (userBalance.length === 0) {
-                res.status(500).json({ error: "Your account doesn't seems to exist, contact an administrator" });
-                return;
-            }
-            //get first element in array with money attribute
-            userBalance = userBalance[0].money;
-
-            // check if user has enough money
-            if (userBalance < itemInfo.price) {
-                res.status(400).json({ error: "You don't have enough money to buy this item." });
-                return;
-            }
+    waterfall([
+        function (waterfallNext) {
+            // get item information
+            getItemInfo(req.params.itemId, waterfallNext);
+        },
+        function (itemInfo, waterfallNext) {
+            // get user's balance
+            getUserBalance(userId, itemInfo, waterfallNext);
+        },
+        function (itemInfo, newBalance, waterfallNext) {
             // update user's balance
-            var newBalance = userBalance - itemInfo.price;
-            updateUserBalance(newBalance, userId).then(function (result) {
-                //if item is a ship
-                if (JSON.parse(itemInfo.specifications).type === "ship") {
-                    // add ship to user ships
-                    addNewShip(itemInfo, userId).then(function (result) {
-                        res.status(200).json({ success: itemInfo.name + " added to your hangar", newBalance: newBalance, itemId: result.insertId });
-                    }, function (error) { mysqlError(res, error) });;
-                }
-                else {
-                    // add item to user's inventory
-                    addItemToInventory(itemInfo, userId).then(function (result) {
-                        res.status(200).json({ success: itemInfo.name + " added to your inventory", newBalance: newBalance, itemId: result.insertId });
-                    }, function (error) { mysqlError(res, error) });;
-                }
-            }, function (error) { mysqlError(res, error) });
-        }, function (error) { mysqlError(res, error) });
-    }, function (error) { mysqlError(res, error) });
+            updateUserBalance(itemInfo, newBalance, userId, waterfallNext);
+        }
+    ],
+        function (error, result) {
+            if (error) {
+                res.status(400).json({ error: error });
+                return;
+            }
+            res.status(200).json(result);
+        });
 }
