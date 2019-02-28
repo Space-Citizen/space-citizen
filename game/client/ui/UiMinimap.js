300591dce5645cd9fcccca2ff0e993f6fa85a530
antoine <antoine.gannat@gmail.com>
2019-02-27 23:01:15 -0800

    add onResize function + minimap: draw line to destination

--- game/client/ui/UiMinimap.js
+++ game/client/ui/UiMinimap.js
@@ -3,41 +3,65 @@ class UiMinimap extends BaseUi {
         this.game = this.state;
         // constants
         this.minimap_margin = 10;
-        this.minimapPosTopLeft = {
+        this.minimap_pos_top_left = undefined;
+        this.minimap_size = undefined;
+        this.minimap_pos_bottom_right = undefined;
+        this.current_destination = undefined;
+        this.current_self_position = undefined;
+        this.setMinimapPosition();
+    }
+
+    setMinimapPosition() {
+        this.minimap_pos_top_left = {
             x: canvas.width - ressources.MINIMAP_BACKGROUND_EARTH.size().x - this.minimap_margin,
             y: canvas.height - ressources.MINIMAP_BACKGROUND_EARTH.size().y - this.minimap_margin
         };
-        this.minimapSize = ressources.MINIMAP_BACKGROUND_EARTH.size();
-        this.minimapPosBottomRight = {
-            x: this.minimapPosTopLeft.x + this.minimapSize.x,
-            y: this.minimapPosTopLeft.y + this.minimapSize.y
+        this.minimap_size = ressources.MINIMAP_BACKGROUND_EARTH.size();
+        this.minimap_pos_bottom_right = {
+            x: this.minimap_pos_top_left.x + this.minimap_size.x,
+            y: this.minimap_pos_top_left.y + this.minimap_size.y
         };
     }
 
     onMouseLeftClick() {
-        // return bool (true to override click)
+        // check if click is in the minimap
+        if (mouse.x >= this.minimap_pos_top_left.x && mouse.y >= this.minimap_pos_top_left.y
+            && mouse.x <= this.minimap_pos_bottom_right.x && mouse.y <= this.minimap_pos_bottom_right.y) {
+            var mousePosInMinimap = {
+                x: mouse.x - this.minimap_pos_top_left.x,
+                y: mouse.y - this.minimap_pos_top_left.y
+            };
+            var worldPos = {
+                x: (Constants.WORLD_SIZE_X * mousePosInMinimap.x) / this.minimap_size.x,
+                y: (Constants.WORLD_SIZE_Y * mousePosInMinimap.y) / this.minimap_size.y
+            };
+            this.game.playerCallFunction("playerMoveTo", worldPos);
+            this.current_destination = { x: mouse.x, y: mouse.y };
+            return (true);
+        }
+        this.current_destination = undefined;
     }
 
     onMouseRightClick() {
-        if (mouse.x >= this.minimapPosTopLeft.x && mouse.y >= this.minimapPosTopLeft.y
-            && mouse.x <= this.minimapPosBottomRight.x && mouse.y <= this.minimapPosBottomRight.y) {
-            var mousePosInMinimap = {
-                x: mouse.x - this.minimapPosTopLeft.x,
-                y: mouse.y - this.minimapPosTopLeft.y
-            };
-            mousePosInMinimap.x = (Constants.WORLD_SIZE_X * mousePosInMinimap.x) / this.minimapSize.x;
-            mousePosInMinimap.y = (Constants.WORLD_SIZE_Y * mousePosInMinimap.y) / this.minimapSize.y;
-            this.game.playerCallFunction("playerMoveTo", mousePosInMinimap);
-            return (true);
-        }
+        // return bool (true to override click)
     }
-    displayViewSize(entityPos) {
-        var viewSize = {
-            x: (this.minimapSize.x * Constants.X_VIEW_RANGE) / Constants.WORLD_SIZE_X,
-            y: (this.minimapSize.y * Constants.X_VIEW_RANGE / Constants.SCREEN_RATIO) / Constants.WORLD_SIZE_Y,
+
+    displayCurrentDestination() {
+        if (!this.current_destination || !this.current_self_position)
+            return;
+        context.beginPath();
+        context.moveTo(this.current_self_position.x, this.current_self_position.y);
+        context.lineTo(this.current_destination.x, this.current_destination.y);
+        context.stroke();
+    }
+
+    displayViewDistance(entityPos) {
+        var viewDistance = {
+            x: (this.minimap_size.x * Constants.X_VIEW_RANGE) / Constants.WORLD_SIZE_X,
+            y: (this.minimap_size.y * Constants.X_VIEW_RANGE / Constants.SCREEN_RATIO) / Constants.WORLD_SIZE_Y,
         };
         context.strokeStyle = "white";
-        context.rect(entityPos.x - viewSize.x / 2, entityPos.y - viewSize.y / 2, viewSize.x, viewSize.y);
+        context.rect(entityPos.x - viewDistance.x / 2, entityPos.y - viewDistance.y / 2, viewDistance.x, viewDistance.y);
         context.stroke();
     }
 
@@ -45,7 +69,7 @@ class UiMinimap extends BaseUi {
         if (!this.game.entities)
             return;
         // display minimap background
-        ressources.MINIMAP_BACKGROUND_EARTH.drawAt(this.minimapPosTopLeft.x, this.minimapPosTopLeft.y);
+        ressources.MINIMAP_BACKGROUND_EARTH.drawAt(this.minimap_pos_top_left.x, this.minimap_pos_top_left.y);
         // for each entity
         for (var index in this.game.entities) {
             const entity = this.game.entities[index];
@@ -64,26 +88,26 @@ class UiMinimap extends BaseUi {
             if (!ressource)
                 continue;
             var entityPos = {
-                x: ((this.minimapSize.x * entity.s_pos.x) / Constants.WORLD_SIZE_X) + this.minimapPosTopLeft.x,
-                y: ((this.minimapSize.y * entity.s_pos.y) / Constants.WORLD_SIZE_Y) + this.minimapPosTopLeft.y
+                x: ((this.minimap_size.x * entity.s_pos.x) / Constants.WORLD_SIZE_X) + this.minimap_pos_top_left.x,
+                y: ((this.minimap_size.y * entity.s_pos.y) / Constants.WORLD_SIZE_Y) + this.minimap_pos_top_left.y
             }
+            // draw the ressource
             ressource.drawCenterAt(entityPos.x, entityPos.y)
+            // if entity is self, draw the view distance
             if (entity.id === this.game.self.id) {
-                this.displayViewSize(entityPos);
+                this.current_self_position = entityPos;
+                this.displayViewDistance(entityPos);
             }
         };
+        this.displayCurrentDestination();
+    }
+    onResize() {
+        console.log("resized");
+        this.setMinimapPosition();
     }
 
     onUpdate(time_elapsed) {
-        this.minimapPosTopLeft = {
-            x: canvas.width - ressources.MINIMAP_BACKGROUND_EARTH.size().x - this.minimap_margin,
-            y: canvas.height - ressources.MINIMAP_BACKGROUND_EARTH.size().y - this.minimap_margin
-        };
-        this.minimapSize = ressources.MINIMAP_BACKGROUND_EARTH.size();
-        this.minimapPosBottomRight = {
-            x: this.minimapPosTopLeft.x + this.minimapSize.x,
-            y: this.minimapPosTopLeft.y + this.minimapSize.y
-        };
+        super.onUpdate(time_elapsed);
         this.displayMinimap();
     }
 }
