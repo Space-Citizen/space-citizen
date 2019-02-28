df0324fc25899384e1f760ebdc98df86d722f9be
Arnaud Alies <arnaudalies.py@gmail.com>
2019-02-28 13:57:32 -0800

    wip

--- game/common/Constants.js
+++ game/common/Constants.js
@@ -2,6 +2,7 @@
 var __PORT = 12345;
 var __WORLD_SIZE = 500;
 var __SCREEN_RATIO = (16 / 9);
+var __X_VIEW_RANGE = 300;
 
 var Constants = {
   FRAMERATE: 30,
@@ -10,7 +11,8 @@ var Constants = {
   WORLD_SIZE_Y: __WORLD_SIZE, // If you want to edit this value world size and background size are related
   PORT: __PORT,
   SCREEN_RATIO: __SCREEN_RATIO,
-  X_VIEW_RANGE: 300, // value in meters. Y view range is defined by screen ratio
+  X_VIEW_RANGE: __X_VIEW_RANGE, // value in meters. Y view range is defined by screen ratio
+  Y_VIEW_RANGE: __X_VIEW_RANGE / __SCREEN_RATIO,
   SOUND_RANGE: 300,
   HEALTH_BAR_SIZE: 30 // value in meters
 }
