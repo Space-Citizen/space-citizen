51125121b61c8b47109d1e0a655de72c7dfb29ed
Morgan SIMON <morgan.simon@epitech.eu>
2019-03-04 17:39:37 -0800

    changelog init

---
+++ website/src/components/changelog/changelog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Changelog extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Changelog</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Changelog
