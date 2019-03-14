8b11de27038bd63e42efc63869af7bde8c8e650f
Arnaud Alies <arnaudalies.py@gmail.com>
2019-03-14 00:38:06 -0700

    Update StateLoading.js

--- game/client/state/StateLoading.js
+++ game/client/state/StateLoading.js
@@ -4,16 +4,38 @@ class StateLoading extends IState {
         context.font = "30px Arial";
     }
 
+    drawLoading(x, y, size, percent) {
+        var x_size = percentWidthToScreen(size);
+        var y_size = x_size / 30;
+
+        var x = x - x_size / 2;
+        var y = y - y_size / 2;
+
+        context.beginPath();
+        context.fillStyle = "grey"
+        context.fillRect(x, y, x_size, y_size);
+        context.stroke();
+
+        context.beginPath();
+        context.fillStyle = "blue"
+        context.fillRect(x, y, x_size * percent / 100, y_size);
+        context.stroke();
+    }
+
     onUpdate(time_elapsed) {
         if (g_loaded_res_counter >= g_res_count) {
             console.log("Ressources loaded");
             return new StateGame();
         }
-        
-        context.fillText("Loading " + g_loaded_res_counter + "/" + g_res_count, 10, 50);
+        this.drawLoading(canvas.width / 2, canvas.height / 2, 80, g_loaded_res_counter / g_res_count * 100);
+        context.fillStyle = "white";
+        context.textAlign = "center";
+        context.font = "30px Verdana";
+        context.fillText("Loading " + g_loaded_res_counter + "/" + g_res_count,
+            canvas.width / 2, canvas.height / 3);
     }
 
     onDestroy() {
-        
+
     }
 }
