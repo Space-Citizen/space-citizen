4c16f4a85babf72d92377a792272d803f991ddb6
Arnaud Alies <arnaudalies.py@gmail.com>
2019-04-04 15:19:35 -0700

    Rework smoothing

--- game/client/entity/BaseEntity.js
+++ game/client/entity/BaseEntity.js
@@ -61,28 +61,16 @@ class BaseEntity {
   }
 
   onUpdate(time_elapsed) {
-
-    var ping_ms = this.average_ping_ms;
-    if (!ping_ms || ping_ms < 50) {
-      // Actually we dont want the entity to teleport when pings gets low
-      ping_ms = 50;
+    var bearing = Helper.getDirection(this.pos, this.s_pos);
+    if (bearing) {
+      var dist = Helper.dist(this.pos, this.s_pos);
+      Helper.moveInDirection(this.pos, bearing, dist * 5, time_elapsed);
     }
-    if (ping_ms > 300) {
-      // We also dont want the entity to stop when ping gets too high
-      ping_ms = 300;
-    }
-    var time_elapsed_ms = time_elapsed * 1000;
-    var multiplier = ((1 / ping_ms) * (1 / time_elapsed_ms)) * 1000;
-    multiplier /= 2;
-    var dir_x = (this.s_pos.x - this.pos.x);
-    var dir_y = (this.s_pos.y - this.pos.y);
-    this.pos.x += dir_x * Math.min(multiplier, 1);
-    this.pos.y += dir_y * Math.min(multiplier, 1);
 
     //var bearing_diff = (this.s_bearing - this.bearing);
     // TODO: smooth bearing
     // TODO: _onUpdate ?
-    if (Helper.onInterval(this, "updateAudio", 0.1) && 0) {
+    if (Helper.onInterval(this, "updateAudio", 0.1)) {
       this._updateAudios();
     }
   }
