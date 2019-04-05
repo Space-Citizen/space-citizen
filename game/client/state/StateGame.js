bee4102e2e075be03aff7c34ee77914c61812576
Arnaud Alies <arnaudalies.py@gmail.com>
2019-04-04 19:10:41 -0700

    Update StateGame.js

--- game/client/state/StateGame.js
+++ game/client/state/StateGame.js
@@ -80,8 +80,12 @@ class StateGame extends IState {
     }
 
     eventUpdateEntities(entities_info) {
-        if (this.last_server_update)
-            this.ups = 1 / ((Date.now() - this.last_server_update) / 1000);
+        if (this.last_server_update) {
+            var dt = Date.now() - this.last_server_update;
+            dt = Math.max(dt, 1); // we dont want to see 'infinite' UPS
+            this.ups = 1 / (dt / 1000);
+        }
+
         this.last_server_update = Date.now();
         //async >> await sleep(1000);
         for (var x in entities_info) {
