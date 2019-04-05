bab72774ce9f7dd4e639eb8089e84dcaf4b45391
Arnaud Alies <arnaudalies.py@gmail.com>
2019-04-04 22:06:01 -0700

    wip regul

--- game/client/entity/BaseEntity.js
+++ game/client/entity/BaseEntity.js
@@ -9,7 +9,7 @@ class BaseEntity {
     this.type = this.s_type;
     this.game.addEntity(this);
     this.killed = false; // var set in listener >> StateGame
-    this.smooth_multiplier = 7;
+    this.smooth_multiplier = 10;
   }
 
   isAlive() {
@@ -66,8 +66,8 @@ class BaseEntity {
     if (bearing) {
       var dist = Helper.dist(this.pos, this.s_pos);
       if (dist > 0.1) {
-        var speed = Helper.regul(this, dist, 0.2, 0.5, 0.4);
-        //console.log(dist * time_elapsed);
+        var speed = Helper.regul(this, dist, 0.2, 0.5, 0.5);
+        //console.log(speed);
         Helper.moveInDirection(
           this.pos,
           bearing,
--- game/common/Helper.js
+++ game/common/Helper.js
@@ -1,18 +1,19 @@
 
 class Helper {
 
-    static regul(self, err, kp, kd, ki) {
+    static regul(self, err, kp, kd, ki, kia = 20) {
         // regulation algorithm
         // self, must be an object which will be used to store variables (use 'this')
         // kp, kd and ki are weight variables
-        // kp short time variation
-        // kd medium time variation
-        // ki long time variation
+        // kp: short time variation
+        // kd: medium time variation
+        // ki: long time variation
+        // kia: number of values used for average
         if (!self.__p_err) {
             self.__err_average = err; // average err
             self.__p_err = err; // previous err
         }
-        self.__err_average = (self.__err_average * 10 + err) / 11;
+        self.__err_average = (self.__err_average * kia + err) / (kia + 1);
         var res = kp * err + kd * (err - self.__p_err) + ki * self.__err_average;
         self.__p_err = err;
         return res;
