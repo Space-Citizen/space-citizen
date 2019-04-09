162725b674fa9079f5846841bd05f1de6ee9ddd9
Morgan SIMON <morgan.simon@epitech.eu>
2019-04-09 14:44:16 -0700

    Add challenges homepage

--- website/src/components/core/coreHome.js
+++ website/src/components/core/coreHome.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ChangeLog from '../changelog/changelog';
 import LeaderBoard from '../leaderboard/leaderboard';
+import Challenge from '../challenge/challenge';
 
 class CoreHome extends Component {
   render() {
     return (
       <div className="container">
+        <Challenge />
         <ChangeLog />
         <LeaderBoard />
       </div>
