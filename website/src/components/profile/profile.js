f8db629cfcc5e6523426eee1e7ca0547f16ec1af
Morgan SIMON <morgan.simon@epitech.eu>
2019-04-09 14:02:40 -0700

    profile

--- website/src/components/profile/profile.js
+++ website/src/components/profile/profile.js
@@ -6,34 +6,38 @@ import "../css/profile.css";
 
 class Profile extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            profile_info: undefined
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      profile_info: undefined
+    };
+  }
 
-    componentDidMount() {
-        get("/api/users/info/" + this.props.match.params.userId).then(response => {
-            this.setState({ profile_info: response.data });
-        });
-    }
-    render() {
-        const { profile_info } = this.state;
+  componentDidMount() {
+    get("/api/users/info/" + this.props.match.params.userId).then(response => {
+      this.setState({ profile_info: response.data });
+    });
+  }
 
-        if (!profile_info)
-            return (<Loading />);
-        return (<div className="row profile-container">
-            {/* User informations  */}
-            <div className="col-2 profile-user-card">
-                <UserCard user={profile_info} />
-            </div>
-            {/* Other informations, posts, etc .. */}
-            <div className="col-10">
+  render() {
+    const { profile_info } = this.state;
 
-            </div>
-        </div>);
-    }
+    if (!profile_info)
+      return (<Loading />);
+    return (
+      <div className="row profile-container">
+        {/* User informations  */}
+        <div className="col-2 profile-user-card mt-3">
+          <UserCard user={profile_info} />
+        </div>
+        {/* Other informations, posts, etc .. */}
+        <div className="col m-3">
+          <p>Level:</p>
+          <p>Time played:</p>
+          <p>Kills:</p>
+        </div>
+      </div>);
+  }
 }
 
 export default Profile;
